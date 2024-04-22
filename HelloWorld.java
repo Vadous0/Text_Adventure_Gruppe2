3aebbf34fd6561bbdb6e9f67c83dcd92f8eeb3e9
JanW <janwiedemann@googlemail.com>
2024-04-22 08:26:26 +0200

    Ausgabe auf .csv geändert

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("Texts/Intro.csv"));
+        scanner.useDelimiter(";");
+
+        while(scanner.hasNext()){
+            System.out.println(scanner.next());
+        }
     }
 }
