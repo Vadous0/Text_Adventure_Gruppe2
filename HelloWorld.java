d3d6fb1a38cb7180531b208648b6e7c0f79663a8
JanW <janwiedemann@googlemail.com>
2024-05-07 12:09:00 +0200

    Kommentare eingefügt

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) throws FileNotFoundException {
-
+        /* Die Klasse Scanner kann neben der Eingabe von Nutzern auch Texte aus Datein einlesen hier z.B. die Datei
+         Intro in dem Ordner Textes mit den Namen Intro.csv.
+         */
         Scanner scanner = new Scanner(new File("Texts/Intro.csv"));
+
+        /* Ein Delimter bestimmt an welches Zeichen ein Text getrennt wird
+         */
         scanner.useDelimiter(";");
 
         while(scanner.hasNext()){
